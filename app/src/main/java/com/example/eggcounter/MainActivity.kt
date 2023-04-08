b1eeab80415a445d7ff5f996e96557d16339afde
Assasin321 <astanmamedov@gmail.com>
2023-04-08 21:48:10 +0600

    created back-end to count and clear digits

--- app/src/main/java/com/example/eggcounter/MainActivity.kt
+++ app/src/main/java/com/example/eggcounter/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.eggcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eggcounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var digit = 0
+
+        binding.eggCircle.setOnClickListener{
+            digit = (digit + 1) % 20
+            binding.eggNumber.text = digit.toString()
+        }
+
+        binding.clearButton.setOnClickListener {
+            digit = 0
+            binding.eggNumber.text = digit.toString()
+        }
+
+
     }
 }
