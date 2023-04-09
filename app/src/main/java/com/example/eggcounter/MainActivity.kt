60c4b06f8ba4b86b10aaa24324bb3ba3f94a5558
Assasin321 <astanmamedov@gmail.com>
2023-04-09 23:30:13 +0600

    add egg shake animation on click and clear digit animation

--- app/src/main/java/com/example/eggcounter/MainActivity.kt
+++ app/src/main/java/com/example/eggcounter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.eggcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.example.eggcounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,11 +17,17 @@ class MainActivity : AppCompatActivity() {
         binding.eggCircle.setOnClickListener{
             digit = (digit + 1) % 20
             binding.eggNumber.text = digit.toString()
+
+            val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
+            binding.eggCircle.startAnimation(shake)
         }
 
         binding.clearButton.setOnClickListener {
             digit = 0
             binding.eggNumber.text = digit.toString()
+
+            val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
+            binding.eggNumber.startAnimation(shake)
         }
 
 
